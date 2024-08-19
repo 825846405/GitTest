cd90e23251f7eab7d0760684831dd78cfbcbf8a0
zccc <825846405@qq.com>
2024-08-19 11:04:29 +0800

    给git测试项目添加输出语句

---
+++ git测试项目/git测试项目/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace git测试项目
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("许淦庭喜欢三步上篮");
+        }
+    }
+}
